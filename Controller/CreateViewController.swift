58a55c1b0f7a76f219d4c8be62da73e962c4b110
bufudash <alanzhu@berkeley.edu>
2020-03-08 16:17:45 -0700

    fixed March8 2020

---
+++ Controller/CreateViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CreateViewController.swift
+//  Lab5SnapagramUI
+//
+//  Created by Lingyue Zhu on 3/8/20.
+//  Copyright © 2020 nyerasi. All rights reserved.
+//
+
+import UIKit
+
+class CreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
