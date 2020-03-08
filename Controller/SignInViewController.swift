1d08ea290702a394c4bc6abeae43835f2cd9291f
grace-lei <slei22@berkeley.edu>
2020-03-08 10:51:29 -0700

    leap of faith

---
+++ Controller/SignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignInViewController.swift
+//  Lab5SnapagramUI
+//
+//  Created by Grace Lei on 3/7/20.
+//  Copyright © 2020 nyerasi. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    let testb = 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
